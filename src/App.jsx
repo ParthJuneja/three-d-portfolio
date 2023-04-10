6aabb7caff81b11a09bba94415e474122ab67bd1
parth <admin@test.com>
2023-04-10 19:39:01 +0530

    changed footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import {About, Contact, Experience, Feedbacks, Hero, Navbar , Tech, Works, StarsCanvas } from './components';
 
 import './App.css'
+import Footer from './components/Footer';
 
 const App = () =>{
 
@@ -20,6 +21,7 @@ const App = () =>{
         <div className="relative z-0">
           <Contact/>
           <StarsCanvas/>
+          <Footer/>
         </div>
       </div>
     // </BrowserRouter>
