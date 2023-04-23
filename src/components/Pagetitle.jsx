acb5b42be8624fc791befc923442914f344ded31
parth <admin@test.com>
2023-04-23 20:19:49 +0530

    changed title color

--- src/components/Pagetitle.jsx
+++ src/components/Pagetitle.jsx
@@ -6,7 +6,7 @@ const Pagetitle = ({ title }) => {
         <div className='px-6'>
             <div class="mt-16 py-5  bg-gray-800 ">
                 <div class="text-start w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-                    <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+                    <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
                         <span class="block">
                             {title}
                         </span>
