2d1161853b4c995437b26cd7932e04053807f2eb
joshikira1 <kiranjoshi597@gmail.com>
2023-04-27 22:08:55 +0530

    lastcomit

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,6 @@ import Privacy from './constants/privacy'
 import Tnc from './constants/Tnc'
 import BonzoTambola from './pages/BonzoTambola/BonzoTambola'
 import ContactUS from './components/ContactUS'
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -19,15 +16,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App/>} />
         {/* <Route path="/#contact" element={<Contact/>} /> */}
         <Route path="/uiux" element={<Topwrapper><Uifeatures /></Topwrapper>} />
+        
         <Route path="/web" element={<Topwrapper><Webdev /></Topwrapper>} />
         <Route path="/mobile" element={<Topwrapper> <Mobiledev /> </Topwrapper>} />
         <Route path="/digital" element={<Topwrapper> <SocialMedia />   </Topwrapper>} />
-        <Route path = "/privacy" element={<Topwrapper> <Privacy/> </Topwrapper>}/>
-        <Route path = "/tnc" element={<Topwrapper> <Tnc/> </Topwrapper>}/>
-        <Route path = "/BonzoTambola" element={<Topwrapper> <BonzoTambola/> </Topwrapper>}/>
-        <Route path = "/ContactUS" element={<Topwrapper> <ContactUS/> </Topwrapper>}/>
-       
-      </Routes>
+        <Route path ="/privacy" element={<Topwrapper> <Privacy/> </Topwrapper>}/>
+        <Route path ="/tnc" element={<Topwrapper> <Tnc/> </Topwrapper>}/>
+        <Route path ="/BonzoTambola" element={<Topwrapper> <BonzoTambola/> </Topwrapper>}/>
+        <Route path ="/ContactUS" element={<Topwrapper> <ContactUS/> </Topwrapper>}/>
+       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 )
